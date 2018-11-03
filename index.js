42a8c05187c19ae7be837b13b1368bb6ec3ba45e
Lost Horizon <ladywitch13@gmail.com>
2018-11-03 20:26:15 +0700

    Update index.js

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ function handleEvent(event) {
 }
 
 // listen on port
-const port = process.env.PORT || 800;
+const port = process.env.PORT || 5000;
 //app.listen(port, () => {
 //  console.log(`listening on ${port}`);
 //});
