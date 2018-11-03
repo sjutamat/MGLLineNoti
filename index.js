0ccbd6430bf2b08dd6d686cdfde13af4d46230e7
Lost Horizon <ladywitch13@gmail.com>
2018-11-03 19:15:23 +0700

    update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 'use strict';
-
+//
 const line = require('@line/bot-sdk');
 const express = require('express');
 var bodyParser = require('body-parser');
@@ -32,38 +32,32 @@ app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 // register a webhook handler with middleware
 // about the middleware, please refer to doc
-app.post('/callback', line.middleware(config), (req, res) => {
-  Promise
-    .all(req.body.events.map(handleEvent))
-    .then((result) => res.json(result));
-	process.on('unhandledRejection', (reason, p) => {
-    console.log('Unhandled Rejection at: Promise', p, 'reason:', reason.stack);
-    // application specific logging, throwing an error, or other logic here
-	console.log(JSON.stringify(req.headers));
-});
-
+app.post('/callback', (req, res) => {
+   console.log(req.body.events[0].source);
+   console.log(req.body.events[0]);
+   res.sendStatus(200);
 });
 
 app.get('/', function (request, response) {
-    response.send('This is MGL Line notification application.');
+    response.send('This is SPWG monitoring application.');
 });
 
-app.get('/mglLine-api', function (req, res) {
+app.get('/spwg-api', function (req, res) {
     const respText = { type: 'text', text: req.query.respText };
-    client.pushMessage('Ccef68d0d971ccfd1ff091808bb24634f', respText);
+    client.pushMessage('C1c764d4eb6a41e516e81dd882ba22ddd', respText);
 	return res.sendStatus(200);
 });
 
-app.post('/mglLine-api', function(req, res) {
+app.post('/spwg-api', function(req, res) {
 	//log line messages
-	  console.log(req.body);
-    console.log(req.body.id);
-    console.log(req.body.system);
-    console.log(req.body.message);
+	//console.log(req.body);
+    //console.log(req.body.id);
+    //console.log(req.body.system);
+    //console.log(req.body.message);
 
 	
     const respText = { type: 'text', text: req.body.message };
-	client.pushMessage('Ccef68d0d971ccfd1ff091808bb24634f', respText);
+	client.pushMessage('C1c764d4eb6a41e516e81dd882ba22ddd', respText);
 	return res.sendStatus(200);
 });
 
@@ -80,12 +74,12 @@ function handleEvent(event) {
 }
 
 // listen on port
-const port = process.env.PORT || 880;
+const port = process.env.PORT || 443;
 //app.listen(port, () => {
 //  console.log(`listening on ${port}`);
 //});
 
-//http.createServer(options,app).listen(port, () => {
+//https.createServer(options, app).listen(port, () => {
  // console.log(`listening on ${port}`);
 //});
 app.listen(port, () => {
